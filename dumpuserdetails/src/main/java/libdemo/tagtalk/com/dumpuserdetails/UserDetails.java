f550619435990e82d2a42b3a6c232d8963347b27
gourav_tagloy <gourav@7588>
2019-05-07 17:19:59 +0530

    Activate Library

---
+++ dumpuserdetails/src/main/java/libdemo/tagtalk/com/dumpuserdetails/UserDetails.java
@@ -0,0 +1,15 @@
+package libdemo.tagtalk.com.dumpuserdetails;
+
+import android.util.Log;
+
+/**
+ * Created by gourav on 5/7/2019.
+ */
+
+public class UserDetails {
+    private static final String TAG = "DumpUserDetails";
+
+    public static void userdata(String sname) {
+        Log.d(TAG, sname);
+    }
+}
